5be7994c3a6af421260005c61f5117f30dabf9f1
RatulAlMamun <ratulalmamun23@gmail.com>
2022-08-11 13:36:36 +0600

    Add Problem no 2557 - R+L=J.

---
+++ 03. Strings/URI 2557 - R+L=J.py
@@ -0,0 +1,11 @@
+while(True):
+    try:
+        data = input()
+        numbers = [i for i in (''.join((ch if ch in '0123456789-' else ' ') for ch in data)).split()]
+        if ('R' in data or 'L' in data):
+            result = int(numbers[1]) - int(numbers[0])
+        else:
+            result = int(numbers[0]) + int(numbers[1])
+        print(result)
+    except EOFError:
+        break
